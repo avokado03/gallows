70886cdc0f6e93e6c5303c9d897d8567b20ae069
Avokado <alotofcode95@gmail.com>
2018-06-24 01:58:16 +0600

    suka ebanyi css

--- js/js.js
+++ js/js.js
@@ -8,10 +8,11 @@ $('#reset').click(function () {
 $('#start').click(function (){
         var alph = "абвгдежзиклмнопрстуфхцчшщъыьэюя";
         var alphDiv="";
+        var alphCurrent;
         var content = $("#content");
         content.empty();
                 for (var i in alph){
-            alphDiv += '<div class="alph">' + alph[i] + '</div>';
+            alphDiv += '<div class="a">' + alph[i] + '</div>';
         }
         content.replaceWith(alphDiv);
         $('#words').css({
@@ -22,20 +23,18 @@ $('#start').click(function (){
             'grid-gap': '0.4em 1.5em'
 
         });
-        $('.alph').css({
+        $('.a').toggleClass("alph");
+        alphCurrent=$('.alph');
+        /*alphCurrent.css({
             'color': '#3f0088',
             'border': '0.09em solid #3f0088',
             'border-radius':'1em',
             'font-family': "'Caveat', cursive",
             'font-size': '2em',
             'background-color': 'rgba(255,255,255,0.5)'
-        });
+        });*/
+        alphCurrent.click(function () {
+            $(this).hide();
+    });
 });
 
-$('.alph').each(
-    function () {
-        $(this).click(function () {
-            $(this).hide();
-        });
-    }
-);
