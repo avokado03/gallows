ae9b0878d58e4be28cd2244026ae0522ecfba0aa
Avokado <alotofcode95@gmail.com>
2018-06-24 14:42:41 +0600

    11

--- js/js.js
+++ js/js.js
@@ -1,40 +1,42 @@
 /**
  * Created by hz on 23.06.2018.
  */
+
+//очистка input
 $('#reset').click(function () {
     $('#name').text("");
 });
-
+//старт игры
 $('#start').click(function (){
-        var alph = "абвгдежзиклмнопрстуфхцчшщъыьэюя";
-        var alphDiv="";
-        var alphCurrent;
-        var content = $("#content");
-        content.empty();
-                for (var i in alph){
-            alphDiv += '<div class="a">' + alph[i] + '</div>';
-        }
-        content.replaceWith(alphDiv);
-        $('#words').css({
-            'padding':'1em',
-            'display':'grid',
-            'grid-template-columns': 'repeat(11, 1fr)',
-            'grid-template-rows':'1fr 1fr 1fr',
-            'grid-gap': '0.4em 1.5em'
+    $('body').css('background', 'url(images/backClean.png)');
 
-        });
-        $('.a').toggleClass("alph");
-        alphCurrent=$('.alph');
-        /*alphCurrent.css({
-            'color': '#3f0088',
-            'border': '0.09em solid #3f0088',
-            'border-radius':'1em',
-            'font-family': "'Caveat', cursive",
-            'font-size': '2em',
-            'background-color': 'rgba(255,255,255,0.5)'
-        });*/
-        alphCurrent.click(function () {
-            $(this).hide();
+    var alph = "абвгдежзиклмнопрстуфхцчшщъыьэюя";
+    var alphDiv="";
+    var alphCurrent;
+    var game=$('#game');
+    var content = $("#content");
+    var appendToGame='<div id="image"></div>' +
+        '<div id="word">' +
+            '<div id="wordBlock"></div>'+
+            '<div id="category"></div>'+
+            '<div id="mistakes"></div>'+
+        '</div>';
+    content.empty();
+    for (var i in alph){
+        alphDiv += '<div class="alph">' + alph[i] + '</div>';
+    }
+    content.append(alphDiv);
+    content.css({
+        'padding':'1em',
+        'display':'grid',
+        'grid-template-columns': 'repeat(11, 1fr)',
+        'grid-template-rows':'1fr 1fr 1fr',
+        'grid-gap': '0.4em 1.5em'
     });
+    alphCurrent=$('.alph');
+    alphCurrent.one('click',function () {
+        $(this).fadeTo(700,0.4);
+    });
+    game.append(appendToGame);
 });
 
