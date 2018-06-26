c26c28c253bb6502dec448eaf257a0483152b4ba
Avokado <alotofcode95@gmail.com>
2018-06-26 22:56:37 +0600

    What is NULL? Baby don't hurt me, don't hurt me, no more...

--- getWords.php
+++ getWords.php
@@ -7,21 +7,21 @@ $query="";
 try{
     $name= $_POST['name'];
     if (isset($name)) {
-        $insName=$connection->prepare("INSERT INTO users VALUES(:username)");
+        $insName=$connection->prepare("INSERT INTO users VALUES(NULL,:username)");
         $insName->execute(array(
             ':username'=>$name
         ));
     }
-    $category = $connection->query("SELECT * FROM categories ORDER BY RAND() LIMIT 0,1");
+    $category = $connection->query("SELECT * FROM `categories` ORDER BY RAND() LIMIT 1");
     $categoryResult=$category->fetch(PDO::FETCH_OBJ);
     $categoryId = $categoryResult->categories_id;
-    $query="SELECT words_id, words_value FROM words WHERE categories_id = ".$categoryId."ORDER BY RAND() LIMIT 0,1";
+    $query="SELECT words_id, words_value FROM words WHERE categories_id = ".$categoryId." ORDER BY RAND() LIMIT 0,1";
     $word=$connection->query($query);
     $wordResult=$word->fetch(PDO::FETCH_OBJ);
     $arrJson=[$categoryResult,$wordResult];
     echo json_encode($arrJson);
 }
 catch (PDOException $ex) {
-    echo "Не удалось выполнить запросы!";
+    echo "Не удалось выполнить запросы!\n";
     echo $ex->getMessage();
 }
